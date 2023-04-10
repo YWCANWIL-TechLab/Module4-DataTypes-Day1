aad0b9490919c4afef22500f0e37cadd4b6f5498
unknown <GrahamE@ywcanwil.org>
2023-04-10 15:25:13 -0500

    Updated

---
+++ scripts/StringExercises.js
@@ -0,0 +1,107 @@
+// * STRING EXERCISES
+// ! For this section, each exercises will be part of a function
+// ! Put your final answer on the return line 
+
+// TODO Change the string below to insert an apostraphe into "Its". Do not change the outer quotation marks.
+function escapePractice() {
+    let escape = 'I love ice cream. Its my favorite dessert.';  // You can get rid of the variable and just return the string when you're done
+}
+
+// TODO Given a random string, return its length
+function findAStringsLength(randomString) {
+
+}
+
+// TODO Given a random string, how would you access the last character in that string? Return your answer
+function accessLastCharacter(string) {
+
+}
+
+// ! Use slice()
+// TODO Given a random url, return everything after the www.
+// Your answer should look something like this: google.com, bing.com, etc
+function getWebsiteName(url) {
+
+}
+
+// ! Use slice()
+// TODO Given a random url, return the http portion and return it. 
+// Example url: https://www.google.com
+function getHttp(url) {
+    
+}
+
+// ! Use substring()
+// TODO Given a random url, return the substring com
+function getCom(url) {
+
+}
+
+// ! Use substr()
+// TODO Given a random url, return the www portion of the string
+function getWww(url) {
+
+}
+
+// Replace the url for bing with google, and return google's url
+function useBetterBrowser() {
+    let browser = "https://www.bing.com";
+
+}
+
+// TODO Given a random string, return that string in all capital letters
+function toUpper(string) {
+
+}
+
+// TODO Given a random string, return that string with all lowercase letters
+function toLower(string) {
+
+}
+
+// TODO Given three strings, combine them using concat()
+function combineStrings(string1, string2, string3) {
+
+}
+
+// TODO Given a string with a lot of whitespace included, return that string without the leading / trailing whitespace
+function trimWhitespace(string) {
+
+}
+
+// TODO Given a string, add whitespace to the start of a string using padStart()
+function addWhitespace(string) {
+
+}
+
+// TODO Given a string, add whitespace to the end of the string using padEnd()
+function addTrailingWhitespace(string) {
+
+}
+
+// TODO Given a random string, return the first character using charAt()
+function getFirstChar(string) {
+
+}
+
+// TODO Given a random string, return the last character's unicode using charCodeAt()
+function getLastCharInUnicode(string) {
+
+}
+
+// TODO Given a random string, return the second character using brackets ([])
+function getSecondChar(string) {
+
+}
+
+// TODO Given a random string, return it as an array with each word as a separate element using split()
+function convertToArray(string) {
+
+}
+
+// TODO Given a random string, return it as an array with each character as a separate element using split()
+function convertToCharArray(string) {
+    
+}
+
+
