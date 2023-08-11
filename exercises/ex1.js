12a8d4d6af1967e6ebe09af25e6b319a90dc1ffa
gseich <gseich@iastate.edu>
2023-08-10 22:29:45 -0500

    Updated Ex1

--- exercises/ex1.js
+++ exercises/ex1.js
@@ -88,127 +88,156 @@ function getCom(url) {
    // TODO Finish the function
 }
 
-// ! Use substr()
-// TODO Given a random url, return the www portion of the string
 /**
  * Given a string of a random url called url, return the "www" portion of the
  * url. 
  * 
  * * Do this using substr()
  * 
+ * ! Do not return a string literal
+ * 
  * @param {String} url 
- * @returns 
+ * @returns {String}
  */
 function getWww(url) {
     // TODO Finish the function
 }
 
 // Replace the url for bing with google, and return google's url
+/**
+ * Given a string called browser, replace the bing portion of the url 
+ * with google.
+ * 
+ * * Do this using replace()
+ * 
+ * @returns {String}
+ */
 function useBetterSearchEngine() {
     let browser = "https://www.bing.com";
-    return browser.replace("bing.com", "google.com");
+    // TODO Finish the function
 }
 
-// TODO Given a random string, return that string in all capital letters
 /**
+ * Given a random string called string, return the same string, but in all 
+ * capital letters.
+ * 
  * 
  * @param {String} string 
+ * @returns {String}
  */
 function toUpper(string) {
-
+    // TODO Finish the function
 }
 
-
-// TODO Given a random string, return that string with all lowercase letters
 /**
+ * Given a random string called string, return the same string, but in all 
+ * lowercase letters.
  * 
- * @param {String} string 
+ * @param {String} string
+ * @returns {String} 
  */
 function toLower(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given three strings, combine them using concat()
 /**
+ * Given three different strings called string1, string2, and string3 
+ * respectively, concatenate all three strings and return the combined string.
  * 
  * @param {String} string1 
  * @param {String} string2 
  * @param {String} string3 
+ * @returns {String}
  */
 function combineStrings(string1, string2, string3) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a string with a lot of whitespace included, return that string without the leading / trailing whitespace
 /**
+ * Given a string with a lot of whitespace included, return that string without 
+ * the leading / trailing whitespace.
  * 
  * @param {String} string 
+ * @returns {String}
  */
 function trimWhitespace(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a string, add whitespace to the start of a string using padStart()
 /**
+ * Given a string called string, add whitespace to the start of the string using
+ *  padStart() and return padded string.
+ * 
  * 
  * @param {String} string 
+ * @returns {String}
  */
 function addWhitespace(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a string, add whitespace to the end of the string using padEnd()
 /**
+ * Given a string called string, add whitespace to the end of the string using 
+ * padEnd() and return the padded string.
  * 
  * @param {String} string 
+ * @returns {String}
  */
 function addTrailingWhitespace(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a random string, return the first character using charAt()
 /**
+ * Given a random string called string, return the first character of the 
+ * string using charAt().
  * 
  * @param {String} string 
+ * @returns {String}
  */
 function getFirstChar(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a random string, return the last character's unicode using charCodeAt()
 /**
+ * Given a random string called string, return the last character's unicode .
  * 
  * @param {String} string 
+ * @returns {Number}
  */
 function getLastCharInUnicode(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a random string, return the second character using brackets ([])
 /**
+ * Given a random string called string, return the second character of the
+ * string using bracket (array) notation.
  * 
  * @param {String} string 
+ * @returns {String}
  */
 function getSecondChar(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a random string, return it as an array with each word as a separate element using split()
 /**
+ * Given a random string called string, return it as an array with each word as 
+ * a separate element using split().
  * 
  * @param {String} string 
+ * @return {String[]}
  */
 function convertToArray(string) {
-
+    // TODO Finish the function
 }
 
-// TODO Given a random string, return it as an array with each character as a separate element using split()
 /**
+ * Given a random string, return it as an array with each character as a separate element using split().
  * 
  * @param {String} string 
+ * @returns {String[]}
  */
 function convertToCharArray(string) {
-
+    // TODO Finish the function
 }
 
 
